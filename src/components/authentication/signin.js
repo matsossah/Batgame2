aa6c203190c4ecdda84a35c42d7aeee68f998545
Mateo Sossah <msossah@batling.fr>
2016-05-06 10:48:07 +0200

    designed login background

--- src/components/authentication/signin.js
+++ src/components/authentication/signin.js
@@ -15,26 +15,37 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   title: {
-    fontSize: 24,
+    fontSize: 28,
+    fontFamily: 'chalkduster',
     color: '#FFD664',
   },
   tagline: {
     fontSize: 16,
+    fontFamily: 'chalkduster',
     color: '#FFD664',
   },
   header: {
     backgroundColor: '#34485E',
-    flex: 25,
+    flex: 30,
+    flexDirection: 'row',
+    alignSelf: 'stretch',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  separator: {
-    flex: 5,
-    backgroundColor: '#FFD664',
   },
   footer: {
     backgroundColor: '#2C3D50',
     flex: 70,
+    alignSelf: 'stretch',
+  },
+  separator: {
+    flex: 1,
+    backgroundColor: '#FFD664',
+  },
+  form: {
+    backgroundColor: '#2C3D50',
+    flex: 29,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   input: {
     padding: 4,
@@ -48,6 +59,7 @@ const styles = StyleSheet.create({
   },
   label: {
     fontSize: 18,
+    fontFamily: 'chalkduster',
   },
   errorMessage: {
     fontSize: 14,
@@ -83,28 +95,29 @@ class Signin extends React.Component {
       <View style={styles.container}>
         <View style={styles.header}>
           <Text style={styles.title}>BATGAME</Text>
-          <Text style={styles.tagline}>Do you have a higher IQ than your friends?!</Text>
         </View>
         <View style={styles.footer}>
           <View style={styles.separator} />
-          <Text>Sign In</Text>
-          <Text style={styles.label}>Username:</Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={(text) => this.setState({ username: text })}
-            value={this.state.username}
-          />
+          <View style={styles.form}>
+            <Text style={styles.label}>Sign In</Text>
+            <Text style={styles.label}>Username:</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={(text) => this.setState({ username: text })}
+              value={this.state.username}
+            />
 
-          <Text style={styles.label}>Password:</Text>
-          <TextInput
-            secureTextEntry
-            style={styles.input}
-            onChangeText={(text) => this.setState({ password: text })}
-            value={this.state.password}
-          />
-          <Text style={styles.errorMessage} >{this.state.errorMessage}</Text>
-          <Button text={'Sign In'} onPress={this.onSigninPress.bind(this)} />
-          <Button text={'I need an account'} onPress={this.onSignupPress.bind(this)} />
+            <Text style={styles.label}>Password:</Text>
+            <TextInput
+              secureTextEntry
+              style={styles.input}
+              onChangeText={(text) => this.setState({ password: text })}
+              value={this.state.password}
+            />
+            <Text style={styles.errorMessage} >{this.state.errorMessage}</Text>
+            <Button text={'Sign In'} onPress={this.onSigninPress.bind(this)} />
+            <Button text={'I need an account'} onPress={this.onSignupPress.bind(this)} />
+          </View>
         </View>
       </View>
     );
