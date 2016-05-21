63185f83b6057afcb9df9d65fd3137f14b3cd73f
Morhaus <alexandre.kirszenberg@gmail.com>
2016-05-21 16:50:30 +0200

    Revert "rnpm link"
    This reverts commit 5bfa99bafce482f595c48f137c002feacb80b0e4.

--- android/app/src/main/java/com/batgame/MainActivity.java
+++ android/app/src/main/java/com/batgame/MainActivity.java
@@ -1,7 +1,6 @@
 package com.batgame;
 
 import com.facebook.react.ReactActivity;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -37,7 +36,6 @@ public class MainActivity extends ReactActivity {
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
-            new FBSDKPackage(),
             new VectorIconsPackage()
         );
     }
