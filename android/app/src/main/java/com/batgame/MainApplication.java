c1f5d61531f5ad295bfd44cc59ffa90aeff2780f
Morhaus <alexandre.kirszenberg@gmail.com>
2016-07-17 18:04:27 +0200

    Add missing files

---
+++ android/app/src/main/java/com/batgame/MainApplication.java
@@ -0,0 +1,52 @@
+package com.batgame;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.facebook.FacebookSdk;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+    private static CallbackManager mCallbackManager = new CallbackManager.Factory().create();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+    }
+
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        protected boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                new MainReactPackage(),
+                new FBSDKPackage(mCallbackManager),
+                new VectorIconsPackage()
+            );
+        }
+    };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+      return mReactNativeHost;
+  }
+
+    public static CallbackManager getCallbackManager() {
+        return mCallbackManager;
+    }
+}
