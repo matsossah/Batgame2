9dcc263fa5c76aedb3ba914f4b2f9f7518ee6e6b
Morhaus <alexandre.kirszenberg@gmail.com>
2016-08-06 12:41:02 +0200

    Request push notification permissions

--- src/StoreProvider.js
+++ src/StoreProvider.js
@@ -3,11 +3,28 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
+import PushNotification from 'react-native-push-notification';
 
 import reducer from './reducers';
 import Main from './Main';
 
 // import codePush from 'react-native-code-push';
+
+PushNotification.configure({
+  onRegister(token) {
+    console.log('TOKEN:', token);
+  },
+
+  onNotification(notification) {
+    console.log('NOTIFICATION:', notification);
+  },
+
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+});
 
 const logger = createLogger();
 
